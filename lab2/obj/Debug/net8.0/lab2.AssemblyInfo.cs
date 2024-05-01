52f4a813f4f29e7349bc2af638e7dc13b92eb71c
nekit <nekitos99956@yandex.ru>
2024-05-01 15:17:28 +0300

    Add

--- lab2/obj/Debug/net8.0/lab2.AssemblyInfo.cs
+++ lab2/obj/Debug/net8.0/lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c6c763b1ba7af2dae510e18a3f024dea4b5832c")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
